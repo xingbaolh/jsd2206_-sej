421d2d739530de409d303196c64f3ffe3efb7b10
tedu <https://gitee.com/xingbaolh/jsd2206_-sej.git>
2022-08-02 12:53:13 +0800

    JSD2206

--- src/exception/AutoCloseDemo2.java
+++ src/exception/AutoCloseDemo2.java
@@ -2,10 +2,17 @@ package exception;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-
+/**
+ * JDK7之后推出了一个新的特性:自动关闭特性
+ *
+ */
 public class AutoCloseDemo2 {
     public static void main(String[] args) {
+        /*
+            自动关闭特性是编译器认可的,而非虚拟机.编译器在编译的时候回将代码改为FinallyDemo2的样子
+         */
         try (
+                //只有实现了AutoCloseable接口的类才可以在try()中定义.JAVA IO中所有的流均实现了该接口
                 FileOutputStream file = new FileOutputStream("fos.txt");
         ) {
             file.write(1);
